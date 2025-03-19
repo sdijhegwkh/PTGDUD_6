e15dc5b778ca331cb75fdebbb734450b4b00b3c9
sdijhegwkh <sprout890@gmail.com>
2025-03-19 11:16:46 +0700

    2 bai dau

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import react, { useReducer, useRef, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const click = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function counterReducer(state, action) {
+  switch (action.type) {
+    case click.INCREMENT:
+      return { count: state.count + 1 };
+    case click.DECREMENT:
+      return { count: state.count - 1 };
+    // case click.MULTIPLY:
+    //   return { count: state.count * state.count };
+    // case click.DIVIDE:
+    //   return { count: state.count / state.count };
+    default:
+      return state;
+  }
 }
 
-export default App
+function InputEx() {
+  const inputRef = useRef();
+  const handleClick = () => {
+    alert("Input value: " + inputRef.current.value);
+  };
+  return (
+    <div>
+      <input ref={inputRef} type="text" placeholder="Nhập gì đó..." />
+      <button onClick={handleClick}>Lấy giá trị</button>
+    </div>
+  );
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+  return (
+    <div>
+      <h1>Count: {state.count}</h1>
+      <button onClick={() => dispatch({ type: click.INCREMENT })}>+</button>
+      <button onClick={() => dispatch({ type: click.DECREMENT })}>-</button>
+    </div>
+  );
+}
+function TodoApp(){
+  const [todos,setTodos]=useState([]);
+  const inputRef=useRef();
+  const addTodo=()=>{
+    const giatri=inputRef.current.value;
+    setTodos([...todos,giatri])
+    inputRef.current.value="";
+  }
+  return(
+    <div>
+      <h1>To do list</h1>
+      <div>
+        <input type="text" placeholder="Nhap cong viec can lam" ref={inputRef} />
+        <button onClick={addTodo}>Them</button>
+      </div>
+      <ul>
+        {todos.map((todo,index) =>(
+          <li key={index}>
+            {todo}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Example() {
+     const [count, setCount] = useState(1);
+      return (
+        <div>
+          <p>Bạn đã click {count} lần</p>
+        <button onClick={() => setCount(count+1)}>
+         Click vào tôi
+       </button>
+       </div>
+    );
+      }
+
+function Bai1_2() {
+  return (
+    <div>
+      <Counter />
+      <InputEx />
+      <TodoApp/>
+      <Example/>
+    </div>
+  );
+}
+
+export default Bai1_2;
